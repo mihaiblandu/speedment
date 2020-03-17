a6628d9e5dd0602460b85d2e43977c9d7de493cc
mihaiblandu <mihaiblandu@icloud.com>
2020-03-17 18:43:39 +0200

    setupdocker

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 docker network create --subnet=172.23.0.0/16 mynet
 
 docker build -t data DBDOKER/
-docker run --net mynet --ip  172.23.0.2 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=sakila -e YSQL_USER=user -e  MYSQL_PASSWORD=password -p 3306:3306 -d data
+docker run --net mynet --ip  172.23.0.2 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=sakila -e YSQL_USER=user -e  MYSQL_PASSWORD=password -p 3306:3306 --name data -d data
 docker build -t speedapp .
-docker run --net mynet --ip  172.23.0.3 -p 8878:8080 -d speedapp
+docker run --net mynet --ip  172.23.0.3 -p 8878:8080 --name speedapp -d speedapp
